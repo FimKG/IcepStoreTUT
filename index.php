ebeacacafb752b16443efb43feb4757b69993ac0
godfrey Timba <gntimba@gmail.com>
2019-07-15 01:18:41 +0200

    changes

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 	<link rel="stylesheet" href="css/bootstrap.css">
 	<!-- Style custom -->
 	<link rel="stylesheet" href="css/style.css">
-		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 	<script src="js/jquery-3.3.1.min.js"></script>
 	<!-- Include all compiled plugins (below), or include individual files as needed -->
 	<script src="js/bootstrap.min.js"></script>
@@ -54,7 +54,13 @@
 			</div>
 		</div>
 		<hr>
+		<h2 class="text-center">Cart</h2>
+
+
+		<div id="table"></div>
+
 	</div>
+
 	<div class="container">
 		<div class="row">
 			<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
@@ -127,12 +133,24 @@
         <abbr title="Phone">Cell:</abbr> (+27) 794 - 2763
       </address>
 			
+
+
+
+
+
+
 				<address>
         <strong>Full Name</strong><br>
 			Kgaugelo Moloto<br>
         <a href="mailto:#">kgaugelob82@gmail.com</a>
         </address>
 			
+
+
+
+
+
+
 			</div>
 		</div>
 	</div>
@@ -147,52 +165,127 @@
 		</div>
 	</footer>
 
-	<script>
-		$( document ).ready( function () {
-	
-			var cart=[]
-			$(this).submit(function(event){
-				event.preventDefault();
-				var button = $( this ) 
-				var id ='#form'+button[0].activeElement.value
-				console.log()
-				let form=$(id).serializeArray()
-				let data={
-					name:form[1].value,
-					price:form[3].value,
-					description:form[0].value,
-					code:form[2].value,
-					qty:form[4].value
-				}
-	
-				cart.push(data)
-				console.table(cart)
-			})
-			
-			
-			
-			
-			
-			$.getJSON( "getproducts.php", function ( result ) {
-				var card='';
-				$.each( result, function ( i, field ) {
-					card += ' <form id="form'+field.code+'"> <div class="col-sm-4 col-md-4 col-lg-4 col-xs-6">';
-					card += '<div class="thumbnail"> <img class="prodImages" src="images/'+field.pic+'" alt="Thumbnail Image 1" class="img-responsive">';
-					card += '<div class="caption">';
-					card += ' <h3>'+field.name+'</h3>';
-					card += ' <p>'+field.description+'</p>'
-					card += ' <p>R'+field.price+'</p>'
-					card += ' <input type="hidden" value="'+field.description+'" name="description" />'
-					card += ' <input type="hidden" value="'+field.name+'" name="name" />'
-					card += ' <input type="hidden" value="'+field.code+'" name="code" />'
-					card += ' <input type="hidden" value="'+field.price+'" name="price" />'
-					card += '<input style="text-align: center" type="number" id="qty" name="qty" value="1" min="0"/><br><br>';
-					card += '<button class="btn btn-primary delete" value="'+field.code+'" > <span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span> Add to Cart</button></div> </div></div></form>';
-				} );
-				$('#tab').html(card);
-			} );
-		} )
-	</script>
-</body>
 
+</body>
+<script>
+	var cart = []
+
+	function deleteCart( index ) {
+		cart.splice( index, 1 )
+		updateCart()
+	}
+			function submitCart() {
+			$.ajax( {
+				url: 'order.php',
+				type: 'post',
+				dataType: "json",
+				data: cart,
+				processData: false,
+				contentType: false,
+				success: function ( data, status ) {
+					console.log( data )
+				},
+				error: function ( xhr, desc, err ) {
+
+
+				}
+			} );
+		} 
+
+	function updateCart() {
+		let table = '<table class="table"><thead class="thead-light"><tr><th scope="col">code</th><th scope="col">Name</th><th scope="col">description</th><th scope="col">Quantity</th><th scope="col">Price</th><th scope="col">Total</th><th scope="col">Remove</th></tr></thead><tbody>';
+		let total = 0;
+		let qty = 0;
+		$.each( cart, function ( i, field ) {
+
+
+			table += '<tr>'
+			table += '<th scope="row">' + field.code + '</th>'
+			table += '<td>' + field.name + '</td>'
+			table += '<td>' + field.description + '</td>'
+			table += '<td>' + field.qty + '</td>'
+			table += '<td>' + field.price + '</td>'
+			table += '<td>R' + field.price * field.qty + '</td>'
+			table += '<td><button class="btn btn-danger delete" onclick="deleteCart(' + i + ')"> <span class="glyphicon glyphicon-trash" aria-hidden="true"></span> Remove</button></td></tr>'
+			qty += field.qty
+			total += ( field.price * field.qty )
+		} )
+		table += '<tr><td></td><td></td>'
+		table += '<th scope="row">Total</th>'
+		table += '<th>' + qty + '</th>'
+		table += '<td></td>'
+		table += '<th>' + total.toFixed( 2 ) + '</th></tr></tbody></table>'
+		table += '<button class="btn btn-success delete" onclick="submitCart()"> <span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span> buy</button>'
+		$( '#table' ).html( table );
+	}
+	$( document ).ready( function () {
+
+
+		$( this ).submit( function ( event ) {
+			event.preventDefault();
+			var button = $( this )
+			var id = '#form' + button[ 0 ].activeElement.value
+			console.log()
+			let form = $( id ).serializeArray()
+			let data = {
+				name: form[ 1 ].value,
+				price: parseFloat( form[ 3 ].value ),
+				description: form[ 0 ].value,
+				code: form[ 2 ].value,
+				qty: parseInt( form[ 4 ].value )
+			}
+			let index = findProduct( cart, 'code', data.code )
+			if ( index == -1 )
+				cart.push( data )
+			else
+				cart[ index ].qty += data.qty
+
+			updateCart()
+			console.table( cart )
+		} );
+
+		function findProduct( array, key, value ) {
+			for ( var i = 0; i < array.length; i++ ) {
+				if ( array[ i ][ key ] === value ) {
+					return i;
+				}
+			}
+			return -1;
+		}
+
+
+
+
+
+
+
+
+		$.getJSON( "getproducts.php", function ( result ) {
+			var card = '';
+			$.each( result, function ( i, field ) {
+				card += ' <form id="form' + field.code + '"> <div class="col-sm-4 col-md-4 col-lg-4 col-xs-6">';
+				card += '<div class="thumbnail"> <img class="prodImages" src="images/' + field.pic + '" alt="Thumbnail Image 1" class="img-responsive">';
+				card += '<div class="caption">';
+				card += ' <h3>' + field.name + '</h3>';
+				card += ' <p>' + field.description + '</p>'
+				card += ' <p>R' + field.price + '</p>'
+				card += ' <input type="hidden" value="' + field.description + '" name="description" />'
+				card += ' <input type="hidden" value="' + field.name + '" name="name" />'
+				card += ' <input type="hidden" value="' + field.code + '" name="code" />'
+				card += ' <input type="hidden" value="' + field.price + '" name="price" />'
+				card += '<input style="text-align: center" type="number" id="qty" name="qty" value="1" min="0"/><br><br>';
+				card += '<button class="btn btn-primary delete" value="' + field.code + '" > <span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span> Add to Cart</button></div> </div></div></form>';
+			} );
+			$( '#tab' ).html( card );
+		} );
+
+
+
+
+
+
+
+
+	} )
+</script>
 </html>
